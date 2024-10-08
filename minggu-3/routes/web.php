4d5c0f73d3368f946d82d42c03aa1f5101327127
iamgymie <6257063+gymie@users.noreply.github.com>
2024-10-08 18:42:32 +0700

    update project minggu-3

---
+++ minggu-3/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\ProdukController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get("/produk", function () {
+//     return view("produk.index");
+// });
+Route::get("/produk", [ProdukController::class, "index"]);
+
+// Route::get("/produk/form", function () {
+//     // return "Ini Halaman Form Produk";
+//     return view("produk.form");
+// });
+Route::get("/produk/form", [ProdukController::class, "form"]);
+
+// Route::get("/sales", function () {
+//     // return "Ini halaman sales";
+//     return view("sales.index");
+// });
+Route::get("/sales", [SalesController::class, "index"]);
